e62662193930649071227f554dc3357f05154254
Tom Wouters <tom.wouters@student.ehb.be>
2020-08-15 17:09:21 +0200

    Final Push

--- Hoofdtaak.js
+++ Hoofdtaak.js
@@ -15,13 +15,13 @@ class Insults {
     this.to = document.getElementById("to");
     this.result = document.getElementById("InsultResult");
     this.addEventListener();
-    
-   
+
+
   }
   addEventListener() {
     this.checkbox.addEventListener("change", this.ImproviseAdaptOvercome.bind(this));
     document.getElementById("submit").addEventListener("click", this.Showresult.bind(this));
-    document.getElementById('generate').addEventListener('click',this.randomResult);
+    document.getElementById('generate').addEventListener('click', this.randomResult);
   }
 
   ImproviseAdaptOvercome() {
@@ -51,23 +51,23 @@ class Insults {
     this.fromVal = this.from.value;
     this.toVal = this.to.value;
     if (this.toVal !== "") {
-      this.htmlString = `<a href="https://foaas.com/${this.BigInsult.value}/${this.fromVal}/${this.toVal}">Here's your insult</a>`;
+      this.htmlString = `<div id="ins"><a href="https://foaas.com/${this.BigInsult.value}/${this.fromVal}/${this.toVal}">Here's your insult</a></div>`;
       Database.saveinsult(this.fromVal, this.BigInsult.value, this.toVal, 0);
     } else {
-      this.htmlString = `<a href="https://foaas.com/${this.LilInsult.value}/${this.fromVal}/">Here's your insult</a>`;
+      this.htmlString = `<div id="ins"><a href="https://foaas.com/${this.LilInsult.value}/${this.fromVal}/">Here's your insult</a></div>`;
       Database.saveinsult(this.fromVal, this.LilInsult.value, this.toVal, 0);
     }
 
     this.result.innerHTML = this.htmlString;
 
   }
-  async randomResult(){
-     var response = await fetch(`https://www.tronalddump.io/search/quote?query=anyone`);
-     var randomData = await response.json();
-     var randomInsult= randomData._embedded.quotes[Math.floor(Math.random() * (randomData._embedded.quotes.length -1 - 0 + 1) + 0)].value
-     document.getElementById('randomResult').innerHTML=`<p>${randomInsult}</p>`
+  async randomResult() {
+    var response = await fetch(`https://www.tronalddump.io/search/quote?query=anyone`);
+    var randomData = await response.json();
+    var randomInsult = randomData._embedded.quotes[Math.floor(Math.random() * (randomData._embedded.quotes.length - 1 - 0 + 1) + 0)].value
+    document.getElementById('randomResult').innerHTML = `<p>${randomInsult}</p>`
   }
-  
+
 }
 
 
@@ -94,8 +94,8 @@ class Firebase {
     this.database.collection("Insults").onSnapshot((doc) => {
       this.data = this.convertQuerySnapshotToRegularArray(doc);
       document.getElementById('message').innerHTML = '';
-      this.data.sort(function(a,b){
-      return a.fromVal.localeCompare(b.fromVal);
+      this.data.sort(function (a, b) {
+        return a.fromVal.localeCompare(b.fromVal);
       })
       this.data.forEach(element => {
         console.log(element);
@@ -134,15 +134,15 @@ class Messages {
   addHtml() {
 
     if (this.to === '') {
-      this.htmlString = `<h3 id="insultMessage">a message has been left by ${this.from}</h3>
+      this.htmlString = `<div id="msglil"><h2 id="insultMessage">a message has been left by ${this.from}</h2>
 <p><a href="${this.url}" id="insultLink">The message</a></p>
 <img id="${this.url}" class="icon" src="./Img/heart.png" alt="heart">
-<p id="count">${this.counter}</p>`
+<p id="count">${this.counter}</p></div>`
     } else {
-      this.htmlString = `<h3 id="insultMessage">a message has been left by ${this.from} for ${this.to}</h3>
+      this.htmlString = `<div id="msgbig"><h2 id="insultMessage">a message has been left by ${this.from} for ${this.to}</h2>
 <p><a href="${this.url}" id="insultLink">The message</a></p>
 <img id="${this.url}" class="icon" src="./Img/heart.png" alt="heart">
-<p id="count">${this.counter}</p>`
+<p id="count">${this.counter}</p></div>`
     }
     document.getElementById('message').insertAdjacentHTML("beforeend", this.htmlString);
   }
